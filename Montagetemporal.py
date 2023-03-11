13e17330cc3e4918430977598ba75234df3c199c
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-03-11 08:41:04 -0800

    Update Montagetemporal.py
    In this example, we first include the Wowza Player JavaScript API script tag in the head of the HTML file. We then create a div element with an ID of player where we will insert the video player.  In the script tag, we create a playerElement variable by selecting the player element using document.getElementById(). We then create a new player instance using the WowzaPlayer.create() method, passing in the playerElement and an object with the player configuration options.  In this case, we specify the Wowza Player license key and the source URL of the live stream we want to play. Note that you would need to replace the license key and source URL with your own values.  Once the player is created, it will automatically start playing the live stream. You can customize the player appearance and behavior using additional configuration options and the Wowza Player API methods.

--- Montagetemporal.py
+++ Montagetemporal.py
@@ -3,3 +3,23 @@ t = t0 + (v / a) * (1 - exp(-a * T))
 In this equation, "t" represents the time at which an event occurs, "t0" represents the initial time, "v" represents the velocity of the object undergoing motion, "a" represents the acceleration of the object, and "T" represents the time interval since the object started moving.
 
 The equation describes the time it takes for the object to reach a certain point, given its initial velocity and acceleration. The term "(1 - exp(-a * T))" represents the time it takes for the object to accelerate to its final velocity.
+
+||
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Wowza Player API Example</title>
+  <script src="https://player.cloud.wowza.com/js/wowzaplayer.min.js"></script>
+</head>
+<body>
+  <div id="player"></div>
+
+  <script>
+    var playerElement = document.getElementById('player');
+    var player = WowzaPlayer.create(playerElement, {
+      "license": "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX",
+      "sourceURL": "https://example.com/live/myStream/playlist.m3u8"
+    });
+  </script>
+</body>
+</html>
