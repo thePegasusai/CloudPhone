53ac2e5e48b4c80f285b13c196a825b85bd84523
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-03-11 08:43:07 -0800

    Update Montagetemporal.py

--- Montagetemporal.py
+++ Montagetemporal.py
@@ -4,8 +4,14 @@ In this equation, "t" represents the time at which an event occurs, "t0" represe
 
 The equation describes the time it takes for the object to reach a certain point, given its initial velocity and acceleration. The term "(1 - exp(-a * T))" represents the time it takes for the object to accelerate to its final velocity.
 
-||
-<!DOCTYPE html>
+||In this example, we first include the Wowza Player JavaScript API script tag in the head of the HTML file. We then create a div element with an ID of player where we will insert the video player.
+
+In the script tag, we create a playerElement variable by selecting the player element using document.getElementById(). We then create a new player instance using the WowzaPlayer.create() method, passing in the playerElement and an object with the player configuration options.
+
+In this case, we specify the Wowza Player license key and the source URL of the live stream we want to play. Note that you would need to replace the license key and source URL with your own values.
+(written in JavaScript) 
+Once the player is created, it will automatically start playing the live stream. You can customize the player appearance and behavior using additional configuration options and the Wowza Player API methods
+<!DOCTYPE html> 
 <html>
 <head>
   <title>Wowza Player API Example</title>
